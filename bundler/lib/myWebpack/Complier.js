dd8ef3700f7d8c18159f29c3fa685d714790e1f4
liurj1 <liurj@yunjiglobal.com>
2020-12-10 19:27:27 +0800

    end

--- bundler/lib/myWebpack/Complier.js
+++ bundler/lib/myWebpack/Complier.js
@@ -1,5 +1,7 @@
 
 const { getAst, getDeps, getCode } = require('./parser')
+const fs = require('fs')
+const path = require('path')
 
 class Complier {
   constructor(options = {}) {
@@ -59,7 +61,7 @@ class Complier {
       }
     },{})
 
-    console.log(depsGraph);
+    this.generate(depsGraph)
     
   }
 
@@ -78,6 +80,38 @@ class Complier {
       deps,//当前文件所有依赖
       code//当前文件解析后的代码
     }
+  }
+  // 生成输出资源
+  generate (depsGraph){
+    const bundle = `
+      (function (depsGraph) {
+        // require目的，为了加载入口文件
+        function require(module){
+          // 定义模块内部require函数
+          function localRequire(relativePath){
+            // 找到要导入模块的绝对路径
+            return require(depsGraph[module].deps[relativePath])
+          }
+
+          // 定义暴露对象（将来模块要暴露的内容都在这里）
+          var exports = {};
+
+          (function(require,exports,code){
+            eval(code)
+          })(localRequire,exports,depsGraph[module].code)
+          // 作为require函数的返回值返回出去
+          // 为了后面的require函数能到到暴露的内容
+          return exports
+        }
+
+        require('${this.options.entry}')
+
+      })(${JSON.stringify(depsGraph)})
+    ` 
+    // 生成输出文件的绝对路径
+    const filePath = path.resolve(this.options.output.path,this.options.output.filename)
+    // 写入文件
+    fs.writeFileSync(filePath,bundle,'utf-8')
   }
 }
 
